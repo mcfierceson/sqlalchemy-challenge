42a7ddf58a488fce5140b817e29a9e92beb88576
Mark McPherson <marksmcpherson@icloud.com>
2020-08-14 23:19:45 -0400

    added flask app

--- app.py
+++ app.py
@@ -22,26 +22,6 @@ Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-
-# Create our session (link) from Python to the DB
-session = Session(engine)
-
-last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-
-# Calculate the date 1 year ago from the last data point in the database
-
-last_date_dt = pd.to_datetime(last_date)
-year_ago = last_date_dt - dt.timedelta(days=366)
-
-# Perform a query to retrieve the data and precipitation scores
-
-results = session.query(Measurement.date, Measurement.prcp).\
-    filter(Measurement.date >= '2016-08-23').\
-    order_by(Measurement.date).all()
-
-# Save the query results as a Pandas DataFrame and set the index to the date column
-
-prcp_df = pd.DataFrame(results, columns=['Date', 'Precipitation'])
 #################################################
 # Flask Setup
 #################################################
@@ -53,10 +33,103 @@ app = Flask(__name__)
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    session = Session(engine)
+
     """Convert the query results to a dictionary using date as the key and prcp as the value"""
+    results = session.query(Measurement.date, Measurement.prcp).all()
 
-    return jsonify(precipitation_dict)
+    session.close()
+    
+    all_precip = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict[f"{date}"] = prcp
+        all_precip.append(prcp_dict)
 
+    return jsonify(all_precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+
+    results = session.query(Station.station, Station.name).all()
+
+    session.close()
+    
+    all_stations = []
+    for station, name in results:
+        station_dict = {}
+        station_dict[f"{station}"] = name
+        all_stations.append(station_dict)
+
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+
+    results = session.query(Measurement.tobs, Measurement.date).\
+        filter(Measurement.date >= '2016-08-23').\
+        filter(Measurement.station == "USC00519397").all()
+
+    session.close()
+
+    tobs_dict = []
+    for tobs, date in results:
+        station_dict = {}
+        station_dict[f"{date}"] = tobs
+        tobs_dict.append(station_dict)
+
+    return jsonify(tobs_dict)
+
+@app.route("/api/v1.0/<start>")
+def start_func(start):
+    session = Session(engine)
+
+    results = session.query(Measurement.date).\
+        group_by(Measurement.date).all()
+
+    all_dates = list(np.ravel(results))
+    
+    if start in all_dates:
+        tobs_results = session.query(Measurement.tobs).\
+            filter(Measurement.date >= start).\
+            group_by(Measurement.tobs).all()
+        temps = list(np.ravel(tobs_results))
+        low = min(temps)
+        high = max(temps)
+        average = round(sum(temps) / len(temps))
+        session.close()
+        return jsonify(f"Minimum temp. is : {low} Max temp is : {high} Average temp is: {average}")
+        
+    session.close()
+    return jsonify({"error": f"{start} not found."}), 404
+
+@app.route("/api/v1.0/<start>/<end>")
+def startend_func(start, end):
+    session = Session(engine)
+
+    results = session.query(Measurement.date).\
+        group_by(Measurement.date).all()
+
+    all_dates = list(np.ravel(results))
+    
+    if (start) in all_dates:
+        if (end) in all_dates: 
+            tobs_results = session.query(Measurement.tobs).\
+                filter(Measurement.date >= start).\
+                filter(Measurement.date <= end).\
+                group_by(Measurement.tobs).all()
+            temps = list(np.ravel(tobs_results))
+            low = min(temps)
+            high = max(temps)
+            average = round(sum(temps) / len(temps))
+            session.close()
+            return jsonify(f"Minimum temp. is : {low} Max temp is : {high} Average temp is: {average}")
+        session.close
+        return jsonify({"error": f"{end} not found."}), 404
+    session.close()
+    return jsonify({"error": f"{start} not found."}), 404
 
 @app.route("/")
 def welcome():
